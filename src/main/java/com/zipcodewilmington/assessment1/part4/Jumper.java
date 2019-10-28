fd8fbb9fc2d839f5936db0ffc50c1ffce3fad13a
wes <wes.jones1597@gmail.com>
2019-10-28 13:08:18 -0400

    Jumper tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,13 @@ public class Jumper {
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
+    public int jumps(int flagHeight, int jumpHeight) {
+        int jumps = 0;
+        if (flagHeight >= jumpHeight) {
+            jumps = (flagHeight / jumpHeight) + (flagHeight % jumpHeight);
+        } else if (flagHeight < jumpHeight) {
+            jumps = flagHeight;
+        }
+        return jumps;
     }
 }
