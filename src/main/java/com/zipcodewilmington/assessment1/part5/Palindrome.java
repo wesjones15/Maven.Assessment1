877e4856d478a3d71b04ff4c6889a09c15680ee4
wes <wes.jones1597@gmail.com>
2019-10-28 13:16:35 -0400

    countPalindrome tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,27 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+        // get all possible substrings
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j < input.length(); j++) {
+                if (isPalindromic(input.substring(i,j+1))) {
+                    count += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isPalindromic(String input) {
+        boolean is_palindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() -1-i)) {
+                is_palindrome = false;
+            }
+        }
+        return is_palindrome;
     }
 }
